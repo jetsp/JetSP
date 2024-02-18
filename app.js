f9e4b9efd12fc62bea269895ba1612a290f3362d
jetsp <jetroe.sanpedro1101@gmail.com>
2024-02-18 18:10:32 +0400

    animationsp1?

--- app.js
+++ app.js
@@ -67,3 +67,53 @@ span.onclick = function() {
       blurBackground.style.display = 'none';
     }
   }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const targetElement = document.querySelector("#typingArea");
+    new Typewriter(targetElement, {
+      loop: false,
+      typeSpeed: 10
+    })
+    .typeString("<span style='color: #ffffff;'>Hello!</span><br>")
+    .typeString("<span style='color: #ffffff;'>My Name's </span>")
+    .typeString("<span style='color: #ff0008;'>Jet Roe</span><br>")
+    .typeString("<span style='color: #ffffff;'>and </span>")
+    .typeString("THIS...")
+    .pauseFor(500)
+    .typeString("<span style='color: #ffffff;'> is my</span> <br>")
+    .typeString("PORTFOLIO!")
+    .start();
+  });
+
+  $(document).ready(function() {
+    if (window.location.hash) {
+        var hash = window.location.hash.substring(1);
+        if ($('#' + hash).length) {
+            $('html, body').animate({
+                scrollTop: $('#' + hash).offset().top
+            }, 500, function() {
+                setTimeout(function() {
+                    window.history.replaceState(null, null, window.location.pathname);
+                }, 1);
+            });
+        } else {
+            $('html, body').scrollTop(0);
+            setTimeout(function() {
+                window.history.replaceState(null, null, window.location.pathname);
+            }, 1);
+        }
+    } else {
+        $('html, body').scrollTop(0);
+    }
+});
+
+const controller = new ScrollMagic.Controller();
+
+const aboutScene = new ScrollMagic.Scene({
+  triggerElement: '#about',
+  triggerHook: 0.1 // trigger the scene when the top of the element is 50% in the viewport
+})
+.setClassToggle('#about', 'about') // add the 'about' class when the scene is triggered
+.addTo(controller);
+
+
