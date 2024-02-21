3e04164b1cbb962df09b5a74fd7430e44484e9be
jetsp <jetroe.sanpedro1101@gmail.com>
2024-02-21 20:20:20 +0400

    animations+content
    ye

--- app.js
+++ app.js
@@ -1,42 +1,95 @@
-let slider = document.querySelector('.slider .list');
-let items = document.querySelectorAll('.slider .list .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-let dots = document.querySelectorAll('.slider .dots li');
+// let slider = document.querySelector('.slider .list');
+// let items = document.querySelectorAll('.slider .list .item');
+// let next = document.getElementById('next');
+// let prev = document.getElementById('prev');
+// let dots = document.querySelectorAll('.slider .dots li');
 
-let lengthItems = items.length - 1;
-let active = 0;
-next.onclick = function(){
-    active = active + 1 <= lengthItems ? active + 1 : 0;
-    reloadSlider();
-}
-prev.onclick = function(){
-    active = active - 1 >= 0 ? active - 1 : lengthItems;
-    reloadSlider();
-}
-let refreshInterval = setInterval(()=> {next.click()}, 3000);
-function reloadSlider(){
-    slider.style.left = -items[active].offsetLeft + 'px';
-    // 
-    let last_active_dot = document.querySelector('.slider .dots li.active');
-    last_active_dot.classList.remove('active');
-    dots[active].classList.add('active');
+// let lengthItems = items.length - 1;
+// let active = 0;
+// next.onclick = function(){
+//     active = active + 1 <= lengthItems ? active + 1 : 0;
+//     reloadSlider();
+// }
+// prev.onclick = function(){
+//     active = active - 1 >= 0 ? active - 1 : lengthItems;
+//     reloadSlider();
+// }
+// let refreshInterval = setInterval(()=> {next.click()}, 3000);
+// function reloadSlider(){
+//     slider.style.left = -items[active].offsetLeft + 'px';
+//     // 
+//     let last_active_dot = document.querySelector('.slider .dots li.active');
+//     last_active_dot.classList.remove('active');
+//     dots[active].classList.add('active');
 
-    clearInterval(refreshInterval);
-    refreshInterval = setInterval(()=> {next.click()}, 3000);
+//     clearInterval(refreshInterval);
+//     refreshInterval = setInterval(()=> {next.click()}, 3000);
 
     
+// }
+
+// dots.forEach((li, key) => {
+//     li.addEventListener('click', ()=>{
+//          active = key;
+//          reloadSlider();
+//     })
+// })
+// window.onresize = function(event) {
+//     reloadSlider();
+// };
+// PICTURE
+
+// Get the cursor element
+var cursor = document.getElementById('cursor');
+
+// Function to update the cursor position and size
+function updateCursorPosition(event) {
+    // Set the cursor position to the current mouse position
+    cursor.style.left = (event.clientX - cursor.offsetWidth / 2) + 'px';
+    cursor.style.top = (event.clientY - cursor.offsetHeight / 2) + 'px';
 }
 
-dots.forEach((li, key) => {
-    li.addEventListener('click', ()=>{
-         active = key;
-         reloadSlider();
-    })
-})
-window.onresize = function(event) {
-    reloadSlider();
-};
+// Function to update the cursor size when hovering over elements with "cursor: pointer"
+function updateCursorSize(event) {
+    // Get the target element
+    var target = event.target;
+
+    // Check if the target element has "cursor: pointer" property
+    if (window.getComputedStyle(target).getPropertyValue('cursor') === 'pointer') {
+        // Increase cursor size by 5px
+        cursor.style.width = (cursor.offsetWidth + 5) + 'px';
+        cursor.style.height = (cursor.offsetHeight + 5) + 'px';
+        // Add pulse animation class
+        cursor.classList.add('pulse');
+    }
+}
+
+// Function to reset cursor size and remove pulse animation
+function resetCursorSize() {
+    // Reset cursor size
+    cursor.style.width = '';
+    cursor.style.height = '';
+    // Remove pulse animation class
+    cursor.classList.remove('pulse');
+}
+
+// Function to add glow effect to the cursor
+function addGlow() {
+    cursor.classList.add('glow');
+}
+
+// Function to remove glow effect from the cursor
+function removeGlow() {
+    cursor.classList.remove('glow');
+}
+
+// Add event listeners to update cursor position, size, and add/remove glow effect
+document.addEventListener('mousemove', updateCursorPosition);
+document.addEventListener('mouseenter', addGlow);
+document.addEventListener('mouseleave', removeGlow);
+document.addEventListener('mouseover', updateCursorSize);
+document.addEventListener('mouseout', resetCursorSize);
+
 
 
 
@@ -50,70 +103,83 @@ var blurBackground = document.getElementById('blurBackground');
 btn.onclick = function() {
   modal.style.display = 'block';
   blurBackground.style.display = 'block';
-  // Add animation class
-  modal.classList.add('animate__zoomIn');
-  blurBackground.classList.add('animate__fadeIn');
 }
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = 'none';
   blurBackground.style.display = 'none';
 }
- // When the user clicks anywhere outside of the modal, close it
- window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = 'none';
-      blurBackground.style.display = 'none';
-    }
-  }
 
   document.addEventListener("DOMContentLoaded", function () {
     const targetElement = document.querySelector("#typingArea");
     new Typewriter(targetElement, {
       loop: false,
-      typeSpeed: 10
+      typeSpeed: 1000
     })
     .typeString("<span style='color: #ffffff;'>Hello!</span><br>")
-    .typeString("<span style='color: #ffffff;'>My Name's </span>")
+    .typeString("<span style='color: #ffffff;'>My Name is </span>")
     .typeString("<span style='color: #ff0008;'>Jet Roe</span><br>")
     .typeString("<span style='color: #ffffff;'>and </span>")
-    .typeString("THIS...")
-    .pauseFor(500)
-    .typeString("<span style='color: #ffffff;'> is my</span> <br>")
+    .typeString("WELCOME")
+    .typeString("<span style='color: #ffffff;'> to my</span> <br>")
     .typeString("PORTFOLIO!")
     .start();
   });
 
-  $(document).ready(function() {
-    if (window.location.hash) {
-        var hash = window.location.hash.substring(1);
-        if ($('#' + hash).length) {
-            $('html, body').animate({
-                scrollTop: $('#' + hash).offset().top
-            }, 500, function() {
-                setTimeout(function() {
-                    window.history.replaceState(null, null, window.location.pathname);
-                }, 1);
-            });
-        } else {
-            $('html, body').scrollTop(0);
-            setTimeout(function() {
-                window.history.replaceState(null, null, window.location.pathname);
-            }, 1);
+  // Function to check if the element is in the viewport
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+      rect.left >= 0 &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to check if the element is in the viewport
+function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+        rect.left >= 0 &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+        rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+}
+(function () {
+	window.onpageshow = function(event) {
+		if (event.persisted) {
+			window.location.reload();
+		}
+	};
+})();
+
+
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector(".glitch").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
         }
-    } else {
-        $('html, body').scrollTop(0);
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
     }
-});
-
-const controller = new ScrollMagic.Controller();
-
-const aboutScene = new ScrollMagic.Scene({
-  triggerElement: '#about',
-  triggerHook: 0.1 // trigger the scene when the top of the element is 50% in the viewport
-})
-.setClassToggle('#about', 'about') // add the 'about' class when the scene is triggered
-.addTo(controller);
-
-
+    
+    iteration += 1 / 3;
+  }, 30);
+}
